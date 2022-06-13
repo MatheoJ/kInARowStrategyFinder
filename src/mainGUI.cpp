57a0410a44a1f83ae421661e5b39b2e49744b63a
MatheoJ <Matheo.joseph.01@gmail.com>
2022-06-13 17:04:32 +0200

    add clickable tile

--- src/mainGUI.cpp
+++ src/mainGUI.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include "TileGenerator.h"
 
 using namespace sf;
@@ -12,21 +13,30 @@ const int WIN_HEIGHT = SIZE_SQUARE*8;
 const int NB_SQUAR_COLUMN = (WIN_HEIGHT)/(WIN_WIDTH/NB_SQUARE_ROW);
 
 
-void InputHandler(Event event, RenderWindow& window);
-void drawTile(RenderWindow& window,Tile tile, int x, int y, int sizeForTile );
-void drawLine(RenderWindow& window, int sizeSquare);
+void InputHandler(Event& event, RenderWindow& window,vector<Tile>& tVcet );
+void drawTile(RenderWindow& window,Tile tile, int x, int y, int sizeForTile,vector<sf::RectangleShape> & shapeTile );
+void drawLine(RenderWindow& window, int sizeSquare, vector<sf::RectangleShape> & shapeLine );
+
 
 int main()
 {
     RenderWindow window(VideoMode(WIN_WIDTH , WIN_HEIGHT), "Tile viewer");
-    /* CircleShape shape(100.f);
-    shape.setFillColor(Color::Green); */
 
     TileGenerator tg;
     vector<char> vect;
     tg.generateBoundWord(6, vect);
     tg.generateTile();
     vector<Tile> tVcet = tg.getTileVect();
+
+    vector<sf::RectangleShape> shapeTile;
+    vector<sf::RectangleShape> shapeLine;
+
+    drawLine(window, SIZE_SQUARE,shapeLine);
+    for (int i=0; i<NB_SQUAR_COLUMN;i++){
+        for(int j =0; j<NB_SQUARE_ROW; j++){
+            drawTile(window, tVcet[i*NB_SQUARE_ROW+j],j*SIZE_SQUARE,i*SIZE_SQUARE,SIZE_SQUARE,shapeTile);
+        }
+    }
    
 
 
@@ -37,17 +47,19 @@ int main()
         {
 
             //input gestion or event 
-            InputHandler(event, window);
+            InputHandler(event, window, tVcet);
         }
 
         window.clear();
-        //window.draw(shape);
-        drawLine(window, SIZE_SQUARE);
-        for (int i=0; i<NB_SQUAR_COLUMN;i++){
-            for(int j =0; j<NB_SQUARE_ROW; j++){
-                drawTile(window, tVcet[i*NB_SQUARE_ROW+j],j*SIZE_SQUARE,i*SIZE_SQUARE,SIZE_SQUARE);
-            }
+
+        for(sf::RectangleShape s: shapeLine ){
+            window.draw(s);
         }
+        
+        for(sf::RectangleShape s: shapeTile ){
+            window.draw(s);
+        }
+        
         window.display();
     }
 
@@ -55,34 +67,45 @@ int main()
 }
 
 
-void InputHandler(Event event, RenderWindow& window){
+void InputHandler(Event& event, RenderWindow& window,vector<Tile>& tVcet){
+
+    static int lastTileClicked =-1;
+
     if (event.type == Event::Closed)
                 window.close();
-
-
+                
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+    {
+        sf::Vector2i localPosition = sf::Mouse::getPosition(window);
+        int x=localPosition.x/SIZE_SQUARE;
+        int y=localPosition.y/SIZE_SQUARE;
+        int num = y*NB_SQUARE_ROW+x;
+        if (num != lastTileClicked){
+            std::cout<<tVcet[num];
+            std::cout<<"x = "<<x<< " y= "<<y<<endl;
+            lastTileClicked = num;
+        }       
+        
+    }
 }
 
-void drawLine(RenderWindow& window, int sizeSquare){
-    
+void drawLine(RenderWindow& window, int sizeSquare, vector<sf::RectangleShape> & shapeLine){
     
     for ( int i =0 ; i<WIN_WIDTH/sizeSquare; i++){
         sf::RectangleShape line(sf::Vector2f(WIN_WIDTH, 3));
         line.setPosition(i*sizeSquare,0);
         line.rotate(90);
-        window.draw(line);
+        shapeLine.push_back(line);
     }
 
     for ( int i =0 ; i<WIN_HEIGHT/sizeSquare; i++){
         sf::RectangleShape line(sf::Vector2f(WIN_HEIGHT, 3));
         line.setPosition(0,i*sizeSquare);
-        window.draw(line);
+        shapeLine.push_back(line);
     }
-
-    
-
 }
 
-void drawTile(RenderWindow& window,Tile tile, int x, int y, int sizeForTile ){
+void drawTile(RenderWindow& window,Tile tile, int x, int y, int sizeForTile, vector<sf::RectangleShape> & shapeTile ){
     int sizeUnit = (sizeForTile-20)/tile.getSize();
     int tileS = tile.getSize();
     Unit** shape = tile.getBasicShape(); 
@@ -97,7 +120,7 @@ void drawTile(RenderWindow& window,Tile tile, int x, int y, int sizeForTile ){
                 u.setPosition(j*sizeUnit+x,i*sizeUnit+y);
                 u.setOutlineColor(Color::Black);
                 u.setOutlineThickness(1);
-                window.draw(u);
+                shapeTile.push_back(u);
             }
         }
     }
